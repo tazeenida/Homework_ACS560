509a9e10514f23b24328ec065bde511cb2b5286b
Nida <tazen01@pfw.edu>
2024-10-17 23:22:21 -0400

    implemented vaadin

---
+++ Movie_Analyzer_Vaadin/src/main/java/com/acs560/Movie_Analyzer_Vaadin/Movie_Analyzer_Vaadin.java
@@ -0,0 +1,13 @@
+package com.acs560.Movie_Analyzer_Vaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movie_Analyzer_Vaadin {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movie_Analyzer_Vaadin.class, args);
+	}
+
+}
